009b39727ee879313dd97cb77143413e1a7eed96
Fabrice Rey <46316372+Fabrice-Rey@users.noreply.github.com>
2023-08-01 17:50:41 +0200

    Update cine-series.js

--- cine-series.js
+++ cine-series.js
@@ -1,6 +1,3 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"></script>
-<script src="https://d3js.org/d3.v6.min.js"></script>
 <script>
   $(document).ready(function () {
     const questionItems = $(".questions");
