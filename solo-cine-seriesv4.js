a2cd2d9b6f1ff5717987818baf436ea5b1b3f5f6
Fabrice Rey <46316372+Fabrice-Rey@users.noreply.github.com>
2023-08-29 01:07:23 +0200

    Update and rename solo-cine-seriesv3.js to solo-cine-seriesv4.js

--- solo-cine-seriesv4.js
+++ solo-cine-seriesv4.js
@@ -1,3 +1,6 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/seedrandom/2.4.4/seedrandom.min.js"></script>
 <script>
 let defiId = null;
 $(document).ready(function () {
